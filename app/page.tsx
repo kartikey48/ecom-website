81a4a00a57861e65765260985d20ff23d657cd53
Kartikey Singh Rana <102845091+kartikey48@users.noreply.github.com>
2025-05-21 14:45:51 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
         {/* Text Block */}
         <div className="flex-1 text-center md:text-left space-y-6">
           <h1 className="text-5xl font-extrabold text-gray-900 leading-tight">
-            Welcome to <span className="text-blue-600">My Ecommerce Course</span>
+            Welcome to <span className="text-blue-600">My Ecommerce </span>
           </h1>
           <p className="text-lg text-gray-600">
             Discover awesome anime t-shirts and more in this fully built ecommerce experience.
